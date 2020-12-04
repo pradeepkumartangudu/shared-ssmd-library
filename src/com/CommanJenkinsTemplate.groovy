a76b67da58a19c8a1ea0a0c902b40677e90f5ac0
Pradeep <pradeepkumartangudu@gmail.com>
2020-12-04 16:43:00 +0530

    ssmd frist commit

---
+++ src/com/CommanJenkinsTemplate.groovy
@@ -0,0 +1,8 @@
+package com
+class CommanJenkinsTemplate{
+Map map_var
+def commanJenkinsTemplate()
+{
+ "mvn "+ map_var.builder.maven.goal
+}
+}
---
+++ vars/config.groovy
@@ -0,0 +1,22 @@
+def call(String filePath, Closure closure){
+def request = libraryResource 'globalconfig.yml'
+def map = readYaml text: request
+
+if(!filePath.isEmpty())
+{
+def file_exist = findfile (glob: filePath)
+if (file_exist.lenght == 1){
+def map1 = readYAML file: filePath
+map.putAll(map1)
+
+}
+}
+CommanJenkinsTemplate comjenkins = new CommanJenkinsTemplate(map_var: map)
+closure.delegate = comjenkins
+closure.resloveStrategy = Closure.DELEGATE_FIRST
+closure()
+}
+
+def call(Closure closure){
+call(''.closure)
+}
