33b4c085d7d2f4595cd673b45dc743fd58b64454
Pradeep <pradeepkumartangudu@gmail.com>
2020-12-05 06:57:25 +0530

    change

--- vars/config.groovy
+++ vars/config.groovy
@@ -2,15 +2,7 @@ def call(String filePath, Closure closure){
 def request = libraryResource 'globalconfig.yml'
 def map = readYaml text: request
 
-if(!filePath.isEmpty())
-{
-def file_exist = findFiles (glob: filePath)
-if (file_exist.lenght == 1){
-def map1 = readYaml file: filePath
-map.putAll(map1)
 
-}
-}
 CommanJenkinsTemplate comjenkins = new CommanJenkinsTemplate(map_var: map)
 closure.delegate = comjenkins
 closure.resloveStrategy = Closure.DELEGATE_FIRST
