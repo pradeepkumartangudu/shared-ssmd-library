51c41c9168f34bedff7a20e7edac2dc188ef03dc
Pradeep <pradeepkumartangudu@gmail.com>
2020-12-05 06:28:51 +0530

    change

--- vars/config.groovy
+++ vars/config.groovy
@@ -1,7 +1,7 @@
 def call(String filePath, Closure closure){
 def request = libraryResource 'globalconfig.yml'
 def map = readYaml text: request
-filePath = map
+filePath = "./resource/globalconfig.yml"
 if(!filePath.isEmpty())
 {
 def file_exist = findFiles (glob: filePath)
